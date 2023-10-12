49cc98c688e46c3f233ad076fc0112a3c7a2e75f
Márk Király <69594158+ProgrammerGnome@users.noreply.github.com>
2023-10-12 17:14:22 +0200

    Add files via upload

---
+++ mintazh/src/main/java/com/example/mintazh/Mapper/BookMapper.java
@@ -0,0 +1,16 @@
+package com.example.mintazh.Mapper;
+
+import com.example.mintazh.Dto.BookDto;
+import com.example.mintazh.Entity.Book;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BookMapper extends EntityMapper<BookDto, Book> {
+
+    // Működik enélkül is!
+    /*
+    Book toEntity(BookDto dto);
+    BookDto toDto(Book book);
+    */
+
+}
---
+++ mintazh/src/main/java/com/example/mintazh/MintazhApplication.java
@@ -0,0 +1,13 @@
+package com.example.mintazh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintazhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MintazhApplication.class, args);
+	}
+
+}
